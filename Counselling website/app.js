6c2787e46c8b97c2ca9b3a8d267a5ec76bf319c3
hj102 <85096230+hj102@users.noreply.github.com>
2023-03-25 00:04:20 +0530

    Initial setup

---
+++ Counselling website/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app=express();
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000,function(){
+    console.log("3000");
+});
+
+
